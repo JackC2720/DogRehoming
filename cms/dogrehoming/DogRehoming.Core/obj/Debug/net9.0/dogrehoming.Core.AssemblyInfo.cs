4bb4e18afe028fa5daff869b31bd40804c48f8c0
Harrie Mayhew <hm@initials.co.uk>
2025-01-11 16:32:30 +0000

    dog list and dog detail models

--- cms/dogrehoming/DogRehoming.Core/obj/Debug/net9.0/dogrehoming.Core.AssemblyInfo.cs
+++ cms/dogrehoming/DogRehoming.Core/obj/Debug/net9.0/dogrehoming.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DogRehoming.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff2b5bd2cd40985bf25fe8dd28cb545e0335d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("DogRehoming.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DogRehoming.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
